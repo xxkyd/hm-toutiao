1ff4260a1876458c22af7d10eac168370dc95a55
kuyedie <1055188318@qq.com>
2019-07-10 16:00:48 +0800

    登录页背景显示和logo

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
+    { name: 'login', path: '/login', component: Login }
   ]// 配置路由规则
 })
 export default router
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="container">
+    <div class="login-container">
+        <el-cord class="login-box">
+            <img src="../../assets/images/logo_index.png" alt="">
+        </el-cord>
     </div>
 </template>
 
@@ -9,6 +12,27 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+    .login-container{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background:url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+        .login-box{
+            width: 400px;
+            height: 300px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+            background: #fff;
+            img{
+                display: block;
+                width: 200px;
+                margin: 10px auto;
+            }
+        }
+    }
 </style>
