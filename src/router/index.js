0f3025621f04970d307809c83fc058df4292fa2d
kuyedie <1055188318@qq.com>
2019-07-14 21:08:03 +0800

    配置文章路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
 import NotFound from '@/views/404'
+import Article from '@/views/article'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
@@ -12,7 +13,8 @@ const router = new VueRouter({
       path: '/',
       component: Home,
       children: [
-        { name: 'welcome', path: '/', component: Welcome }
+        { name: 'welcome', path: '/', component: Welcome },
+        { name: 'article', path: '/article', component: Article }
       ] },
     { name: '404', path: '*', component: NotFound }
   ]// 配置路由规则
