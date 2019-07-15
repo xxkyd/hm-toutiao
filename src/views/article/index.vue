17786341b619007b30712786525fe9a1585262d1
kuyedie <1055188318@qq.com>
2019-07-15 08:33:34 +0800

    内容管理-筛选条件布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,15 +1,70 @@
 <template>
-    <div class="article-container">
-        Login
-    </div>
+  <div class="article-container">
+    <el-card>
+      <div slot="header">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form :model="reqParams" size="small" label-width="80px">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="reqParams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="1">草稿</el-radio>
+            <el-radio :label="2">待审核</el-radio>
+            <el-radio :label="3">审核通过</el-radio>
+            <el-radio :label="4">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <el-select v-model="reqParams.channel_id" placeholder="所有频道">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="时间：">
+          <el-date-picker
+            v-model="dataValues"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card></el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      reqParams: {
+        status: null,
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
+      },
+      //   频道的选项数组
+      channelOptions: [{ name: 'java', id: 1 }],
+      dataValues: []
+    }
+  }
 }
 </script>
 
 <style>
-
+.el-card {
+  margin-bottom: 10px;
+}
 </style>
